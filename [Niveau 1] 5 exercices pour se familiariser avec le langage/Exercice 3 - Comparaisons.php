5914f25006736f51d711220ec8124d9e5d41ca69
Aiiishaaa <aicha.hamida06@yahoo.fr>
2020-10-02 16:29:59 +0200

    2nd Commit

--- [Niveau 1] 5 exercices pour se familiariser avec le langage/Exercice 3 - Comparaisons.php
+++ [Niveau 1] 5 exercices pour se familiariser avec le langage/Exercice 3 - Comparaisons.php
@@ -44,7 +44,8 @@ function lePlusPetit( $arg1,$arg2,$arg3){
     }
     else  if (( $arg2 > $arg1) && ( $arg2 > $arg3)) {
           if( $arg1 > $arg3) {
-            return $arg3 ; }
+            return $arg3 ; 
+        }
         else { 
             return $arg1;
        }
